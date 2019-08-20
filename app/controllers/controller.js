6f8c40ea3aaa85e8bffac7181e16c070efd0da1f
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-08-20 13:02:16 -0600

    changed file structure

--- app/controllers/controller.js
+++
@@ -1,7 +0,0 @@
-let express = require("express");
-let router = express.Router();
-let home = require('app/views/portfolio/src/app.js');
-
-router.get("/", (req, res) => {  
-        res.send(home)
-});
