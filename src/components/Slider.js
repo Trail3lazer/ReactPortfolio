19537b6b0bcb124f3c6511c6bb7ae17776cc5121
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-09-24 15:59:09 -0600

    made it easier to read

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,33 +4,34 @@ import Button from "react-bootstrap/Button"
 import "../CSS/slider.css"
 
 let picArray = [
-    "https://d1p0m2aj917ekc.cloudfront.net/slider/C97A4562.jpg", 
-    "https://d1p0m2aj917ekc.cloudfront.net/slider/IMG_7554-photo.png", 
-    "https://d1p0m2aj917ekc.cloudfront.net/slider/IMG_7582-photo.JPG", 
+    "https://d1p0m2aj917ekc.cloudfront.net/slider/C97A4562.jpg",
+    "https://d1p0m2aj917ekc.cloudfront.net/slider/IMG_7554-photo.png",
+    "https://d1p0m2aj917ekc.cloudfront.net/slider/IMG_7582-photo.JPG",
     "https://d1p0m2aj917ekc.cloudfront.net/slider/C97A4446.jpg"
 ]
 
 const Slider = (props) => {
-        return (
-            <React.Fragment>
-                <Carousel>
-                    {picArray.map((value, index) => {
-                        return <Carousel.Item key={index}>
-                            <img
-                                className="img-fluid"
-                                src={value}
-                                alt={index}
-                            />
-                            <Carousel.Caption>
-                                <Button variant="dark" className="aboutBtn"
-                                onClick={props.modal}>About Me</Button>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    })}
-                </Carousel>
-            </React.Fragment>
-        );
-    }
+    return (
+            <Carousel>
+                {picArray.map((value, index) => {
+                    return <Carousel.Item key={index}>
+                        <img
+                            className="img-fluid"
+                            src={value}
+                            alt={index}
+                        />
+                        <Carousel.Caption>
+                            <Button variant="dark"
+                                className="aboutBtn"
+                                onClick={props.modal}>
+                                About Me
+                                </Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                })}
+            </Carousel>
+    );
+}
 
 
 export default Slider;
